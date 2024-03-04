636b61b64a52e96a90cae3de08c47decb8cf8743
ChenDangUnicaNWPU <126569540+ChenDangUnicaNWPU@users.noreply.github.com>
2024-03-04 15:11:11 +0100

    Update run_alignment.sh

--- run_alignment.sh
+++ run_alignment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python run_alignment_evaluation.py  --n_surrogates 2 --batch_size 2 --device cpu
+python run_alignment_evaluation.py  --n_surrogates 20 --batch_size 1000 --device cuda
