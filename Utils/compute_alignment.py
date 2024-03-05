81c153d45448fc95040d589910667f46210d0461
Raffaele Mura <raffaele.mura@unica.it>
2024-03-05 18:42:31 +0100

    Fixed compute_alignment.py

--- Utils/compute_alignment.py
+++ Utils/compute_alignment.py
@@ -5,25 +5,23 @@ from config import SURROGATE_NAMES
 
 def compute_alignment(input, victim_model, ens_surrogates, loss_fn, target):
     alignment_dict = {}
-    input = input.unsqueeze(dim=0)
-    victim_model.eval()
-    input.requires_grad_()
-    input_tensor = normalize(input / 255)
-    outputs = victim_model(input_tensor)
+    image = image.unsqueeze(dim=0)
+    image.requires_grad_()
+    #input_tensor = normalize(image / 255)
+    outputs = victim_model(image)
     lossv = loss_fn(outputs, target)
-    lossv.backward(retain_graph=True)
+    lossv.backward()
     with torch.no_grad():
-        vic_grad = input.grad.detach()
+        vic_grad = image.grad.clone().detach()
+        g1 = torch.flatten(vic_grad)
 
     for i, model in enumerate(ens_surrogates):
-        input.grad.data.zero_()
-        model.eval()
-        outputs = model(input_tensor)
+        #image.grad.data.zero_()
+        outputs = model(image)
         loss = loss_fn(outputs, target)
-        loss.backward(retain_graph=True)
+        loss.backward()
         with torch.no_grad():
-            surr_grad = input.grad.detach()
-            g1 = torch.flatten(vic_grad)
+            surr_grad = image.grad.detach()
             g2 = torch.flatten(surr_grad)
             alignment = torch.dot(g1, g2) / torch.norm(g1, p=2) * torch.norm(g2, p=2)
             alignment_dict[SURROGATE_NAMES[i]] = alignment.item()
