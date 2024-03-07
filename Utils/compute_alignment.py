344959a3387950878d4f04d8c93e9314ac448ae4
Raffaele Mura <rff.mura@gmail.com>
2024-03-07 15:53:23 +0100

    Fixed Alignment computation

--- Utils/compute_alignment.py
+++ Utils/compute_alignment.py
@@ -21,7 +21,7 @@ def compute_alignment(image, victim_model, ens_surrogates, loss_fn, target):
         with torch.no_grad():
             surr_grad = image.grad.detach()
             g2 = torch.clone(torch.flatten(surr_grad))
-            alignment = torch.dot(g1, g2) / torch.norm(g1, p=2) * torch.norm(g2, p=2)
+            alignment = torch.dot(g1, g2) / (torch.norm(g1, p=2) * torch.norm(g2, p=2))
             alignment_dict[SURROGATE_NAMES[i]] = alignment.item()
 
     return alignment_dict
